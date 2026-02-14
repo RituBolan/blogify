f9a951a61c57983e56ecaa0e2fd3f3cf5a8a54f9
Ritu Bolan <bolanritu@gmail.com>
2026-02-14 12:39:07 +0530

    first commit

---
+++ models/users.js
@@ -0,0 +1,71 @@
+const { createHmac, randomBytes } = require("crypto");
+const { Schema, model } = require("mongoose");
+const { createTokenForUser, } = require("../services/auth");
+
+const userSchema=new Schema({
+    name:{
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    salt:{
+        type: String,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    profileImageURL: {
+        type: String,
+        default: "/images/default.jpg",
+    },
+    role:{
+        type: String,
+        enum : ["USER","ADMIN"],
+        default: "USER",
+    },
+
+},{ timestamps: true});
+
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+
+  const salt = randomBytes(16).toString("hex");
+
+  const hashedPassword = createHmac("sha256", salt)
+    .update(this.password)
+    .digest("hex");
+
+  this.salt = salt;
+  this.password = hashedPassword;
+});
+
+
+userSchema.statics.matchPasswordAndGenerateToken= async function(email,password){
+    const user=await this.findOne({email});
+    if(!user) throw new  Error('User not found'); 
+
+    const salt=user.salt;
+    const hashedPassword=user.password;
+
+
+    const userProvideHash= createHmac("sha256",salt)
+    .update(password).digest("hex");
+
+
+    if( hashedPassword!== userProvideHash) throw new Error("Incorrect Password");
+
+    
+const token = createTokenForUser(user);
+
+    return token;
+};
+
+
+const User=model("user",userSchema);
+
+module.exports=User;
---
+++ routes/user.js
@@ -0,0 +1,44 @@
+const {Router}= require("express");
+const User=require("../models/users");
+
+const router=Router();
+
+router.get("/signin", (req,res)=>{
+    return res.render("signin");
+});
+
+router.get("/signup", (req,res)=>{
+    return res.render("signup");
+});
+
+router.post("/signin",async (req,res)=>{
+    try{
+    const {email, password}= req.body;
+    const token= await User.matchPasswordAndGenerateToken(email,password);
+
+    
+    return res.cookie("token",token).redirect("/");
+    }
+    catch(error){
+        return res.render("signin",{
+            error: "Incorrect email or password"
+        });
+    }
+    
+});
+
+router.get("/logout", (req,res)=>{
+    res.clearCookie('token').redirect("/");
+})
+
+router.post("/signup", async (req,res)=>{
+    const {name, email, password}=req.body;
+    await User.create({
+        name,
+        email,
+        password,
+    });
+    return res.redirect("/");
+});
+
+module.exports = router;
